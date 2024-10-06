c3f1b3e31fb5fbb22f3ea1335e497b31986da6a0
mslms <misael.4375@gmail.com>
2024-10-06 00:03:44 -0300

    added terminal window motions

--- lua/keymaps/remap.lua
+++ lua/keymaps/remap.lua
@@ -5,17 +5,23 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Exit file to netrw" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search highlights" })
-vim.keymap.set("n", "<C-c>", "<C-\\><C-n>", { desc = "Exit terminal mode" });
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" });
 
 -- better window navigation
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-h>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- terminal mode
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w><C-k>", { desc = "Move focus to the upper window" })
+
 -- split windows
+vim.keymap.set("n", "<leader>ws", "<C-w><C-s>", { desc = "Window split horizontal" })
 vim.keymap.set("n", "<leader>wv", ":vsplit<CR>", { desc = "Window split vertical" })
-vim.keymap.set("n", "<leader>wh", ":split<CR>", { desc = "Window split horizontal" })
 
 -- in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move marked text" })
